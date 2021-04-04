aeab6e47466df1f9ee0ca6116bbc5116f9d55715
Kirill Kuzmin <kirik550088@gmail.com>
2021-04-04 15:23:06 +0300

    Changes with color

--- app.py
+++ app.py
@@ -5,23 +5,26 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from random import randint
 from kivy.core.window import Window
-Window.size = (550, 500)
-Window.clearcolor = (randint(0, 255)/255, randint(0, 255)/255, randint(0, 255)/255, 1)
-Window.title = "Test"
+
+
+Window.size = (1000, 1000)
+Window.clearcolor = (10/255, 201/255, 1/255,1)
+Window.title = "Weather_Now"
 
 class WeatherApp(App):
 
     def __init__(self):
         super().__init__()
-        self.label = Label(text='Weather')
-        self.input_data = TextInput(hint_text='В каком месте вы хотите узнать погоду?: ', multiline=False)
-        self.output_data = Label(text='В вашем городе:')
+        self.label = Label(text='Weather', font_size=24)
+        self.input_data = TextInput(hint_text='В каком месте вы хотите узнать погоду?: ', multiline=False, font_size=24)
+        self.output_data = Label(text='В вашем городе:', font_size=24)
         self.input_data.bind(text=self.GetText)
 
+    def update_label(self, instance):
+        self.lbl.text = str(instance.text)
 
     def GetText(self, *args):
         data = self.input_data.text
-
 
 
 
@@ -37,6 +40,10 @@ class WeatherApp(App):
 
 
 
+
         return box
 
+
 WeatherApp().run()
+
+
