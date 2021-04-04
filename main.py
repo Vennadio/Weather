3df58b8e1d7e25b0ef8659adfaaa70e4cea580a8
EstonianGuy <74711970+EstonianGuy@users.noreply.github.com>
2021-04-04 14:16:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
-resp = requests.get('https://world-weather.ru/pogoda/russia/moscow/24hours/', headers=header).text
+resp = requests.get('https://world-weather.ru/pogoda/russia/', headers=header).text
 soup = BS(resp, 'html.parser')
-weather = soup.find_all('table', class_= "weather-today")
-for data in weather:
-    if data.find('td', class_ = 'weather-feeling' ):
-        print (data)
 
-#title = soup.find('table', class_='day hourly-1').text
-
-#print(title.strip())
-#print(weather)
-
+for link in soup.find(class_="block-cities").find_all('a'):
+print(link.text)
+print(link.get('href'))
 
